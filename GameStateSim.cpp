f15844f14f5cf86544ea4a2b72f5289efe19c6a6
SigmaOverdrive <rodrigocortesst@gmail.com>
2025-06-20 23:12:00 -0400

    balatro

--- GameStateSim.cpp
+++ GameStateSim.cpp
@@ -1,12 +1,10 @@
 #include "GameStateSim.h"
 
-// Implementación del constructor de SimPersonaje.
 SimPersonaje::SimPersonaje(int id_, int hp_, int maxHp_, int vel_, int dmg_, bool vivo_, int ctrl_)
     : id(id_), hp(hp_), maxHp(maxHp_), velocidad(vel_),
-    dañoBase(dmg_), vivo(vivo_), control(ctrl_), posicion(0) {
+    daÃ±oBase(dmg_), vivo(vivo_), control(ctrl_), posicion(0) {
 }
 
-// Implementación de la función `estaTerminado()`.
 bool GameStateSim::estaTerminado() const {
     bool iaViva = false;
     bool jugadorVivo = false;
@@ -20,7 +18,6 @@ bool GameStateSim::estaTerminado() const {
     return !(iaViva && jugadorVivo);
 }
 
-// Implementación de la función `avanzarTurno()`.
 void GameStateSim::avanzarTurno() {
     int total = personajes.size();
     for (int i = 1; i <= total; ++i) {
