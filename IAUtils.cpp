792f8d8a9694911ebc2c055a8627235738502842
SigmaOverdrive <rodrigocortesst@gmail.com>
2025-06-20 23:14:09 -0400

    aaaaaaaaaa

--- IAUtils.cpp
+++ IAUtils.cpp
@@ -6,7 +6,6 @@ std::vector<GameStateSim> generarAcciones(const GameStateSim& estadoActual) {
     const SimPersonaje& atacante = estadoActual.personajes[estadoActual.indiceTurnoActual];
 
     if (!atacante.vivo) {
-        // Personaje muerto, solo pasa el turno
         GameStateSim siguiente = estadoActual;
         siguiente.avanzarTurno();
         posiblesEstados.push_back(siguiente);
@@ -16,22 +15,18 @@ std::vector<GameStateSim> generarAcciones(const GameStateSim& estadoActual) {
     for (size_t i = 0; i < estadoActual.personajes.size(); ++i) {
         const SimPersonaje& objetivo = estadoActual.personajes[i];
 
-        // Solo objetivos enemigos y vivos
         if (objetivo.vivo && objetivo.control != atacante.control) {
             GameStateSim nuevoEstado = estadoActual;
 
-            // Aplicar daño
             SimPersonaje& objetivoEnEstado = nuevoEstado.personajes[i];
-            objetivoEnEstado.hp -= atacante.dañoBase;
+            objetivoEnEstado.hp -= atacante.daÃ±oBase;
             if (objetivoEnEstado.hp <= 0) {
                 objetivoEnEstado.hp = 0;
                 objetivoEnEstado.vivo = false;
             }
 
-            // Avanzar turno
             nuevoEstado.avanzarTurno();
 
-            // Guardar nuevo estado
             posiblesEstados.push_back(nuevoEstado);
         }
     }
